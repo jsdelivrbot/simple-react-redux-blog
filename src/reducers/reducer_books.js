bd61a99792f9d6599510c1d7b008b01073020c46
Thushara <smtsampath@hotmail.com>
2017-03-24 17:30:56 -0400

    React Redux Mapping

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'Harry Poter' },
+    { title: 'The Dark Tower' },
+    { title: 'Cracking the Coding Interview' },
+    { title: 'The Self-Taught Programmer' }
+  ]
+}
